564408d7174bd021059815729ec4c1e29b309423
56thfk <56thfk@gmail.com>
2021-05-23 22:17:44 +0900

    성별 설정

--- app/src/main/java/com/example/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -1,38 +1,62 @@
 package com.example.mainactivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ArrayList<String> al;
-    private ArrayAdapter<String> arrayAdapter;
+    private cards cards_data[];
+    private arrayAdapter arrayAdapter;
     private int i;
+
+    private FirebaseAuth mAuth;
+    private String currentUId;
+    private DatabaseReference userDb;
+
+    ListView listView;
+    List<cards> rowItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        al = new ArrayList<>();
-        al.add("php");
-        al.add("c");
-        al.add("python");
-        al.add("java");         //카드의 이름들
-        al.add("html");
-        al.add("c++");
-        al.add("css");
-        al.add("javascript");
+        userDb = FirebaseDatabase.getInstance().getReference().child("Users");
 
-        arrayAdapter = new ArrayAdapter<>(this, R.layout.item, R.id.helloText, al ); //카드마다 al의 텍스트를 출력, item은 텍스트뷰, 색깔
+        mAuth = FirebaseAuth.getInstance();
+        currentUId = mAuth.getCurrentUser().getUid();
+        
+
+        checkUserSex();
+
+
+
+        rowItems = new ArrayList<cards>(); // 카드 배열들
+
+        arrayAdapter = new arrayAdapter(this, R.layout.item, rowItems ); //카드마다 al의 텍스트를 출력, item은 텍스트뷰, 색깔
+
+        //al.add("java") <- 실행은 되지만 이것만 쓰면 앱 상에선 출력이 되질않음
+        //arrayAdapter.notifyDataSetChanged(); <- 변경되었다는 것을 써야함
 
         SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
 
@@ -43,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
             public void removeFirstObjectInAdapter() {
                 // this is the simplest way to delete an object from the Adapter (/AdapterView)
                 Log.d("LIST", "removed object!");
-                al.remove(0);
+                rowItems.remove(0);
                 arrayAdapter.notifyDataSetChanged();
             }
 
@@ -66,11 +90,6 @@ public class MainActivity extends AppCompatActivity {
             //카드를 다 소진 시켰을 때
             @Override
             public void onAdapterAboutToEmpty(int itemsInAdapter) {
-                // Ask for more data here
-                al.add("XML ".concat(String.valueOf(i)));
-                arrayAdapter.notifyDataSetChanged();
-                Log.d("LIST", "notified");
-                i++;
             }
 
             @Override
@@ -87,6 +106,96 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Click!", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+    private String userSex; // 사용자의 성별
+    private String userSexOpp; // 사용자의 반대 성별
+    public void checkUserSex() {
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
+        // maleDb의 이벤트리스너
+        DatabaseReference maleDb = FirebaseDatabase.getInstance().getReference().child("Users").child("Male");
+        maleDb.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                if (snapshot.getKey().equals(user.getUid())) {
+                    userSex = "Male";
+                    userSexOpp = "Female";
+                    getOppositeSexUsers();
+                }
+            }
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+            }
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+        //femaleDb의 이벤트리스너
+        DatabaseReference femaleDb = FirebaseDatabase.getInstance().getReference().child("Users").child("FeMale");
+        femaleDb.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                if (snapshot.getKey().equals(user.getUid())) {
+                    userSex = "Female";
+                    userSexOpp = "Male";
+                    getOppositeSexUsers();
+                }
+            }
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+            }
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+    //반대 성별의 유저함수
+    public void getOppositeSexUsers() {
+        DatabaseReference oppositeSexDb = FirebaseDatabase.getInstance().getReference().child("Users").child(userSexOpp);
+        oppositeSexDb.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+               if(snapshot.exists()) {
+                   cards item = new cards(snapshot.getKey(), snapshot.child("name").getValue().toString()); //반대 성별의 name을 카드 배열에 추가한다
+                   rowItems.add(item);
+                   arrayAdapter.notifyDataSetChanged();
+               }
+            }
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+            }
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+    //로그아웃
+    public void logoutUser(View view) {
+    mAuth.signOut();
+    Intent intent = new Intent(MainActivity.this, ChooseLoginRegistrationActivity.class); // MainActivity -> ChooseLoginRegistrationActivity
+    startActivity(intent);
+    finish();
+    return;
     }
 }
--- app/src/main/java/com/example/mainactivity/RegistrationActivity.java
+++ app/src/main/java/com/example/mainactivity/RegistrationActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -15,11 +17,15 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     private Button mRegister;
-    private EditText mEmail, mPassword;
+    private EditText mEmail, mPassword, mName;
+
+    private RadioGroup mRadioGroup;
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
@@ -46,21 +52,36 @@ public class RegistrationActivity extends AppCompatActivity {
 
         mEmail = findViewById(R.id.email);
         mPassword = findViewById(R.id.password);
+        mName = findViewById(R.id.name);
 
-        mRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final String email = mEmail.getText().toString();
-                final String password = mPassword.getText().toString();
-                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (!task.isSuccessful()) {
-                            Toast.makeText(RegistrationActivity.this, "sign up error", Toast.LENGTH_SHORT).show();
-                        }
+        mRadioGroup = findViewById(R.id.radioGroup);
+
+        mRegister.setOnClickListener(view -> {
+            int selectId = mRadioGroup.getCheckedRadioButtonId(); // 라디오버튼 체크 확인
+
+            final RadioButton radioButton = (RadioButton) findViewById(selectId);
+
+            if(radioButton.getText() == null) {
+                return;
+            } // 성별을 정하지 않을 경우 이 리스너는 여기서 종료
+
+            final String email = mEmail.getText().toString();
+            final String password = mPassword.getText().toString();
+            final String name = mName.getText().toString();
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(RegistrationActivity.this, "sign up error", Toast.LENGTH_SHORT).show();
                     }
-                });
-            }
+                    else {
+                        String userId = mAuth.getCurrentUser().getUid(); //파이어베이스 Authentication의 User ID를 가져옴
+                        DatabaseReference currentUserDb = FirebaseDatabase.getInstance().getReference().child("Users").child(radioButton.getText().toString()).child(userId).child("name");
+                                                                //DB의 Users 노드 -> 라디오버튼에 해당하는 성별 노드(Female, Male) -> (사용자의 ID에 기반한 노드 생성) ->name 노드
+                        currentUserDb.setValue(name); // 사용자가 입력한 이름을 DB의 name노드에 저장함
+                    }
+                }
+            });
         });
     }
 
